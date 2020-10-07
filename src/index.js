8a80ed86f738494f93ae48c1958d4bc95a37b69f
Elbembilo <Elbembilo@gmail.com>
2020-10-07 18:01:40 +0300

    0

--- src/index.js
+++ src/index.js
@@ -5,25 +5,38 @@ import "bootstrap";
 import Chart from "chart.js";
 import "./body.json";
 import axios from "axios";
-axios.get("/src/body.json").then((res) => {
-  console.log(res.data);
-});
 
-let lebelsYears = axios.get("/src/body.json").then((res) => {
-  let years = years.map((item) => item.year);
-});
+async function getPersent() {
+  const response = await axios.get("/src/body.json").then((response) => {
+    return response.data.years.map((item) => item.persent);
+  });
+  const data = await response;
+  console.log(data);
+}
+
+async function getYear() {
+  const response = await axios.get("/src/body.json").then((response) => {
+    return response.data.years.map((item) => item.year);
+  });
+  const data = await response;
+  console.log(response);
+}
+// let getYearsLabels = axios.get("/src/body.json").then((response) => {
+//   return response.data.years.map((item) => item.year);
+// });
+// getYearsLabels.then((data) => {
+//   console.log(data);
+// });
 
 let ctx = document.getElementById("myChart");
-let requestURL = "./body.json";
 let myChart = new Chart(ctx, {
   type: "line",
   data: {
-    labels: [lebelsYears],
+    labels: getYear(),
     datasets: [
       {
         label: "90% к 2015 году",
-        lable: "95% к 2020 году",
-        data: [36, 26, 40, 42, 44, 46, 59, 80, 81, 82, 85, 84, 45, 50, 90],
+        data: getPersent(),
         backgroundColor: ["#0d49cc"],
         borderColor: ["#0d49cc"],
         borderWidth: 2,
