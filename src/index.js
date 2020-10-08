b8912f22379321971d9ffc99217833f2ab7017bb
Elbembilo <Elbembilo@gmail.com>
2020-10-08 18:38:04 +0300

    0

--- src/index.js
+++ src/index.js
@@ -6,52 +6,53 @@ import Chart from "chart.js";
 import "./body.json";
 import axios from "axios";
 
-async function getPersent() {
-  const response = await axios.get("/src/body.json").then((response) => {
-    return response.data.years.map((item) => item.persent);
-  });
-  const data = await response;
-  console.log(data);
+// async function getPersent() {
+//   const response = await axios.get("/src/body.json").then((response) => {
+//     return response.data.years.map((item) => item.persent);
+//   });
+
+//   console.log(response);
+// }
+
+getResponceData();
+// statisticsRes();
+async function getResponceData() {
+  const response = await axios.get("/src/body.json");
+  const year = response.data.years.map((item) => item.year);
+  const persent = response.data.years.map((item) => item.persent);
+
+  Chartjson(year, persent);
+  // console.log(year);
+  // console.log(persent);
+  // statisticsRes(response);
 }
 
-async function getYear() {
-  const response = await axios.get("/src/body.json").then((response) => {
-    return response.data.years.map((item) => item.year);
-  });
-  const data = await response;
-  console.log(response);
-}
-// let getYearsLabels = axios.get("/src/body.json").then((response) => {
-//   return response.data.years.map((item) => item.year);
-// });
-// getYearsLabels.then((data) => {
-//   console.log(data);
-// });
-
-let ctx = document.getElementById("myChart");
-let myChart = new Chart(ctx, {
-  type: "line",
-  data: {
-    labels: getYear(),
-    datasets: [
-      {
-        label: "90% к 2015 году",
-        data: getPersent(),
-        backgroundColor: ["#0d49cc"],
-        borderColor: ["#0d49cc"],
-        borderWidth: 2,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      yAxes: [
+function Chartjson(year, persent) {
+  let ctx = document.getElementById("myChart");
+  let myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: year,
+      datasets: [
         {
-          ticks: {
-            beginAtZero: true,
-          },
+          label: "95% к 2020 году",
+          data: persent,
+          backgroundColor: ["#0d49cc"],
+          borderColor: ["#0d49cc"],
+          borderWidth: 2,
         },
       ],
     },
-  },
-});
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+  });
+}
